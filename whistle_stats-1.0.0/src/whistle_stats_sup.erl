a48f0be8f5acb14307b02a294f0cac6fc673c142
Stephen Gibberd <stephen@finder.com.kh>
2013-08-06 10:26:22 +0700

    KAZOO-200 Add SNMP monitioring of cluster statistics

--- whistle_stats-1.0.0/src/whistle_stats_sup.erl
+++ whistle_stats-1.0.0/src/whistle_stats_sup.erl
@@ -1,29 +1,26 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2012, VoIP, INC
+%%% @copyright (C) 2012-2013, 2600Hz
 %%% @doc
 %%%
 %%% @end
 %%% @contributors
-%%% Jon Blanton <jon@2600hz.com>
 %%%-------------------------------------------------------------------
 -module(whistle_stats_sup).
 
 -behaviour(supervisor).
 
--include_lib("whistle/include/wh_types.hrl").
-
 -export([start_link/0]).
--export([listener_proc/0]).
 -export([init/1]).
 
+-include_lib("whistle/include/wh_types.hrl").
+
+%% Added to work with old verison of wh_types.hrl
+-ifndef(WORKER).
+-define(WORKER(I), {I, {I, 'start_link', []}, 'permanent', 5000, 'worker', [I]}).
+-endif.
+
 %% Helper macro for declaring children of supervisor
--define(CHILD(Name, Type), fun(N, cache) -> {N, {wh_cache, start_link, [N]}, permanent, 5000, worker, [wh_cache]};
-                              (N, T) -> {N, {N, start_link, []}, permanent, 5000, T, [N]} end(Name, Type)).
-%% -define(CHILDREN, [{skel_listener, worker}, {skel_cache, cache}]).
--define(CHILDREN, [%{folsom_sup, supervisor}
-		   %,{whistle_stats_couchbeam, worker}
-		   %,{whistle_stats_statsd, worker}
-		  ]).
+-define(CHILDREN, [?WORKER('whistle_stats')]).
 
 %% ===================================================================
 %% API functions
@@ -35,15 +32,9 @@
 %% Starts the supervisor
 %% @end
 %%--------------------------------------------------------------------
--spec start_link/0 :: () -> startlink_ret().
+-spec start_link() -> startlink_ret().
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
-
--spec listener_proc/0 :: () -> {'ok', pid()}.
-listener_proc() ->
-    [P] = [P || {Mod, P, _, _} <- supervisor:which_children(?MODULE),
-                Mod =:= whistle_stats_listener],
-    {ok, P}.
+    supervisor:start_link({'local', ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
@@ -60,11 +51,10 @@ listener_proc() ->
 %%--------------------------------------------------------------------
 -spec init([]) -> sup_init_ret().
 init([]) ->
-    RestartStrategy = one_for_one,
+    RestartStrategy = 'one_for_one',
     MaxRestarts = 5,
     MaxSecondsBetweenRestarts = 10,
 
     SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
-    Children = [?CHILD(Name, Type) || {Name, Type} <- ?CHILDREN],
 
-    {ok, {SupFlags, Children}}.
+    {'ok', {SupFlags, ?CHILDREN}}.
