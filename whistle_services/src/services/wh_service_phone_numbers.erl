meksiu/kazoo

KAZOO-3888: Money maker

core/whistle_services-1.0.0/src/services/wh_service_phone_numbers.erl
@@ -12,10 +12,36 @@
 -export([phone_number_activation_charge/2]).
 
 -include("../whistle_services.hrl").
--include_lib("whistle_number_manager/include/wh_number_manager.hrl").
+-include_lib("kazoo_number_manager/include/knm_phone_number.hrl").
 
 -define(PHONE_NUMBERS, <<"phone_numbers">>).
 -define(NUMBER_SERVICES, <<"number_services">>).
+-define(LISTING_BY_NUMBER, <<"numbers/list_by_number">>).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec reconcile(wh_services:services()) -> wh_services:services().
+-spec reconcile(wh_json:objects(), wh_services:services()) -> wh_services:services().
+reconcile(Services) ->
+    AccountId = wh_services:account_id(Services),
+    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
+    case couch_mgr:get_results(AccountDb, ?LISTING_BY_NUMBER, ['include_docs']) of
+        {'error', _R} ->
+            lager:debug("unable to get current phone numbers in service: ~p", [_R]),
+            Services;
+        {'ok', JObjs} ->
+            Docs = [wh_json:get_value(<<"doc">>, JObj) || JObj <- JObjs],
+            reconcile(Docs, Services)
+    end.
+
+reconcile(JObjs, Services) ->
+    S1 = wh_services:reset_category(?PHONE_NUMBERS, Services),
+    S2 = wh_services:reset_category(?NUMBER_SERVICES, S1),
+    update_numbers(JObjs, S2).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -47,27 +73,10 @@ phone_number_activation_charge(Number, Services) ->
             wht_util:dollars_to_units(Charge)
     end.
 
-%%--------------------------------------------------------------------
-%% @public
-%% @doc
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec reconcile(wh_services:services()) -> wh_services:services().
-reconcile(Services) ->
-    AccountId = wh_services:account_id(Services),
-    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
-    case couch_mgr:open_doc(AccountDb, ?WNM_PHONE_NUMBER_DOC) of
-        {'error', _R} ->
-            lager:debug("unable to get current phone_numbers in service: ~p", [_R]),
-            Services;
-        {'ok', JObj} -> reconcile(JObj, Services)
-    end.
 
-reconcile(PhoneNumbers, Services) ->
-    S1 = wh_services:reset_category(?PHONE_NUMBERS, Services),
-    S2 = wh_services:reset_category(?NUMBER_SERVICES, S1),
-    update_numbers(wh_json:get_keys(wh_json:public_fields(PhoneNumbers)), PhoneNumbers, S2).
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 
 %%--------------------------------------------------------------------
 %% @private
@@ -75,21 +84,22 @@ reconcile(PhoneNumbers, Services) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec update_numbers(ne_binaries(), wh_json:object(), wh_services:services()) -> wh_services:services().
-update_numbers([], _, Services) ->
+-spec update_numbers(wh_json:objects(), wh_services:services()) -> wh_services:services().
+update_numbers([], Services) ->
     Services;
-update_numbers([Number|Numbers], JObj, Services) ->
+update_numbers([JObj|JObjs], Services) ->
+    Number = wh_doc:id(JObj),
     case wnm_util:is_reconcilable(Number) of
         'false' -> Services;
         'true' ->
-            Routines = [fun(S) -> update_number_quantities(Number, S, JObj) end
+            Routines = [fun(S) -> update_number_quantities(JObj, S) end
                         ,fun(S) ->
-                                 Features = wh_json:get_value([Number, <<"features">>], JObj, []),
-                                 update_feature_quantities(Features, S)
+                            Features = wh_json:get_value(?PVT_FEATURES, JObj, wh_json:new()),
+                            update_feature_quantities(Features, S)
                          end
                        ],
             UpdatedServices = lists:foldl(fun(F, S) -> F(S) end, Services, Routines),
-            update_numbers(Numbers, JObj, UpdatedServices)
+            update_numbers(JObjs, UpdatedServices)
     end.
 
 %%--------------------------------------------------------------------
@@ -98,9 +108,10 @@ update_numbers([Number|Numbers], JObj, Services) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec update_number_quantities(ne_binary(), wh_services:services(), wh_json:object()) -> wh_services:services().
-update_number_quantities(Number, Services, JObj) ->
-    ModuleName = wh_json:get_atom_value([Number, <<"module_name">>], JObj),
+-spec update_number_quantities(wh_json:object(), wh_services:services()) -> wh_services:services().
+update_number_quantities(JObj, Services) ->
+    Number = wh_doc:id(JObj),
+    ModuleName = wh_json:get_atom_value(?PVT_MODULE_NAME, JObj),
     case is_number_billable(Number, ModuleName) andalso
         wnm_util:classify_number(Number)
     of
@@ -111,6 +122,12 @@ update_number_quantities(Number, Services, JObj) ->
             wh_services:update(?PHONE_NUMBERS, Classification, Quantity + 1, Services)
     end.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec is_number_billable(ne_binary(), api_atom()) -> boolean().
 is_number_billable(DID, 'undefined') ->
     case catch wnm_number:get(DID) of
@@ -142,7 +159,7 @@ is_number_billable(DID, Module) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec update_feature_quantities(ne_binaries(), wh_services:services()) -> wh_services:services().
+-spec update_feature_quantities(ne_binaries() | wh_json:object(), wh_services:services()) -> wh_services:services().
 update_feature_quantities([], Services) ->
     Services;
 update_feature_quantities([?DASH_KEY|Features], Services) ->
@@ -152,4 +169,6 @@ update_feature_quantities([?VITELITY_KEY|Features], Services) ->
 update_feature_quantities([Feature|Features], Services) ->
     Quantity = wh_services:updated_quantity(?NUMBER_SERVICES, Feature, Services),
     UpdatedServices = wh_services:update(?NUMBER_SERVICES, Feature, Quantity + 1, Services),
-    update_feature_quantities(Features, UpdatedServices).
+    update_feature_quantities(Features, UpdatedServices);
+update_feature_quantities(JObj, Services) ->
+    update_feature_quantities(wh_json:get_keys(JObj), Services).

icehess/kazoo-core

whistle_services/src/services/wh_service_phone_numbers.erl
@@ -12,10 +12,37 @@
 -export([phone_number_activation_charge/2]).
 
 -include("whistle_services.hrl").
--include_lib("whistle_number_manager/include/wh_number_manager.hrl").
+-include_lib("kazoo_number_manager/include/knm_phone_number.hrl").
 
 -define(PHONE_NUMBERS, <<"phone_numbers">>).
 -define(NUMBER_SERVICES, <<"number_services">>).
+-define(LISTING_BY_NUMBER, <<"numbers/list_by_number">>).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec reconcile(wh_services:services()) -> wh_services:services().
+-spec reconcile(wh_json:objects(), wh_services:services()) -> wh_services:services().
+
+reconcile(Services) ->
+    AccountId = wh_services:account_id(Services),
+    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
+    case kz_datamgr:get_results(AccountDb, ?LISTING_BY_NUMBER, ['include_docs']) of
+        {'error', _R} ->
+            lager:debug("unable to get current phone numbers in service: ~p", [_R]),
+            Services;
+        {'ok', JObjs} ->
+            Docs = [wh_json:get_value(<<"doc">>, JObj) || JObj <- JObjs],
+            reconcile(Docs, Services)
+    end.
+
+reconcile(JObjs, Services) ->
+    S1 = wh_services:reset_category(?PHONE_NUMBERS, Services),
+    S2 = wh_services:reset_category(?NUMBER_SERVICES, S1),
+    update_numbers(JObjs, S2).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -47,27 +74,9 @@ phone_number_activation_charge(Number, Services) ->
             wht_util:dollars_to_units(Charge)
     end.
 
-%%--------------------------------------------------------------------
-%% @public
-%% @doc
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec reconcile(wh_services:services()) -> wh_services:services().
-reconcile(Services) ->
-    AccountId = wh_services:account_id(Services),
-    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
-    case kz_datamgr:open_doc(AccountDb, ?WNM_PHONE_NUMBER_DOC) of
-        {'error', _R} ->
-            lager:debug("unable to get current phone_numbers in service: ~p", [_R]),
-            Services;
-        {'ok', JObj} -> reconcile(JObj, Services)
-    end.
-
-reconcile(PhoneNumbers, Services) ->
-    S1 = wh_services:reset_category(?PHONE_NUMBERS, Services),
-    S2 = wh_services:reset_category(?NUMBER_SERVICES, S1),
-    update_numbers(wh_json:get_keys(wh_json:public_fields(PhoneNumbers)), PhoneNumbers, S2).
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 
 %%--------------------------------------------------------------------
 %% @private
@@ -75,21 +84,22 @@ reconcile(PhoneNumbers, Services) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec update_numbers(ne_binaries(), wh_json:object(), wh_services:services()) -> wh_services:services().
-update_numbers([], _, Services) ->
+-spec update_numbers(wh_json:objects(), wh_services:services()) -> wh_services:services().
+update_numbers([], Services) ->
     Services;
-update_numbers([Number|Numbers], JObj, Services) ->
+update_numbers([JObj|JObjs], Services) ->
+    Number = wh_doc:id(JObj),
     case wnm_util:is_reconcilable(Number) of
         'false' -> Services;
         'true' ->
-            Routines = [fun(S) -> update_number_quantities(Number, S, JObj) end
+            Routines = [fun(S) -> update_number_quantities(JObj, S) end
                         ,fun(S) ->
-                                 Features = wh_json:get_value([Number, <<"features">>], JObj, []),
-                                 update_feature_quantities(Features, S)
+                            Features = wh_json:get_value(?PVT_FEATURES, JObj, wh_json:new()),
+                            update_feature_quantities(Features, S)
                          end
                        ],
             UpdatedServices = lists:foldl(fun(F, S) -> F(S) end, Services, Routines),
-            update_numbers(Numbers, JObj, UpdatedServices)
+            update_numbers(JObjs, UpdatedServices)
     end.
 
 %%--------------------------------------------------------------------
@@ -98,9 +108,10 @@ update_numbers([Number|Numbers], JObj, Services) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec update_number_quantities(ne_binary(), wh_services:services(), wh_json:object()) -> wh_services:services().
-update_number_quantities(Number, Services, JObj) ->
-    ModuleName = wh_json:get_atom_value([Number, <<"module_name">>], JObj),
+-spec update_number_quantities(wh_json:object(), wh_services:services()) -> wh_services:services().
+update_number_quantities(JObj, Services) ->
+    Number = wh_doc:id(JObj),
+    ModuleName = wh_json:get_atom_value(?PVT_MODULE_NAME, JObj),
     case is_number_billable(Number, ModuleName) andalso
         wnm_util:classify_number(Number)
     of
@@ -111,6 +122,12 @@ update_number_quantities(Number, Services, JObj) ->
             wh_services:update(?PHONE_NUMBERS, Classification, Quantity + 1, Services)
     end.
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
 -spec is_number_billable(ne_binary(), api_atom()) -> boolean().
 is_number_billable(DID, 'undefined') ->
     case catch wnm_number:get(DID) of
@@ -142,7 +159,7 @@ is_number_billable(DID, Module) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec update_feature_quantities(ne_binaries(), wh_services:services()) -> wh_services:services().
+-spec update_feature_quantities(ne_binaries() | wh_json:object(), wh_services:services()) -> wh_services:services().
 update_feature_quantities([], Services) ->
     Services;
 update_feature_quantities([?DASH_KEY|Features], Services) ->
@@ -152,4 +169,6 @@ update_feature_quantities([?VITELITY_KEY|Features], Services) ->
 update_feature_quantities([Feature|Features], Services) ->
     Quantity = wh_services:updated_quantity(?NUMBER_SERVICES, Feature, Services),
     UpdatedServices = wh_services:update(?NUMBER_SERVICES, Feature, Quantity + 1, Services),
-    update_feature_quantities(Features, UpdatedServices).
+    update_feature_quantities(Features, UpdatedServices);
+update_feature_quantities(JObj, Services) ->
+    update_feature_quantities(wh_json:get_keys(JObj), Services).